e5b6da3dfb2c0d9f07fbfcea40fa6586b848c9e1
Venkat Kabadi <167209161+Venkat-Kabadi@users.noreply.github.com>
2024-09-28 21:50:49 +0530

    Add files via upload

---
+++ Weight Convertor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+weight=float(input("Enter your weight: "))
+unit=input("Kilograms or Pounds (K or L): ")
+
+if unit=="K":
+    weight=weight*2.205
+    unit="Lbs."
+    print(f"Your weight is: {round(weight,2)} {unit}")
+    
+elif unit=="L":
+    weight=weight/2.205
+    unit="Kilograms."
+    print(f"Your weight is: {round(weight,2)} {unit}")
+else:
+    print(f"{unit} is not valid")
+
+
+# In[ ]:
+
+
+
+
